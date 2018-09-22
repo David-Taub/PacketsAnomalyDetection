458e972149ad8a95bb68e755af865b04667ecfa7
David Ariel <davidariel333@gmail.com>
2018-09-22 23:33:59 +0300

    fixed incomplete batch at end of data bug fixed paths

--- train.py
+++ train.py
@@ -15,12 +15,12 @@ from keras.models import Model
 from keras import regularizers
 
 
-TXT_FILEPATH = r'C:\Users\booga\Dropbox\projects\argus\Argus_Protocol_Research\reference_recording_01.txt'
-ENCODER_MODEL_FILEPATH = r'C:\Users\booga\Dropbox\projects\argus\Argus_Protocol_Research\encoder.h5'
-# X_TRAIN_MM_FILEPATH = r'C:\Users\booga\Dropbox\projects\argus\Argus_Protocol_Research\x_train.mm'
-# Y_TRAIN_MM_FILEPATH = r'C:\Users\booga\Dropbox\projects\argus\Argus_Protocol_Research\y_train.mm'
-# X_VALIDATION_MM_FILEPATH = r'C:\Users\booga\Dropbox\projects\argus\Argus_Protocol_Research\x_validation.mm'
-# Y_VALIDATION_MM_FILEPATH = r'C:\Users\booga\Dropbox\projects\argus\Argus_Protocol_Research\y_validation.mm'
+TXT_FILEPATH = r'C:\Users\booga\Dropbox\projects\PacketsAnomalyDetection\Argus_Protocol_Research\reference_recording_01.txt'
+ENCODER_MODEL_FILEPATH = r'C:\Users\booga\Dropbox\projects\PacketsAnomalyDetection\Argus_Protocol_Research\encoder.h5'
+# X_TRAIN_MM_FILEPATH = r'C:\Users\booga\Dropbox\projects\PacketsAnomalyDetection\Argus_Protocol_Research\x_train.mm'
+# Y_TRAIN_MM_FILEPATH = r'C:\Users\booga\Dropbox\projects\PacketsAnomalyDetection\Argus_Protocol_Research\y_train.mm'
+# X_VALIDATION_MM_FILEPATH = r'C:\Users\booga\Dropbox\projects\PacketsAnomalyDetection\Argus_Protocol_Research\x_validation.mm'
+# Y_VALIDATION_MM_FILEPATH = r'C:\Users\booga\Dropbox\projects\PacketsAnomalyDetection\Argus_Protocol_Research\y_validation.mm'
 ENCODING_DIM = 40
 SLIDING_WINDOW_WIDTH = 20
 
@@ -30,7 +30,7 @@ def generator(data, window_size, batch_size):
     while True:
         for i in range(int(np.ceil((data.shape[0] - window_size) / batch_size))):
             y = data[i * batch_size + window_size - 1: (i + 1) * batch_size + window_size - 1, :]
-            x = np.zeros((batch_size, window_size - 1, features))
+            x = np.zeros((y.shape[0], window_size - 1, features))
             for j in range(batch_size):
                 x[j, :, :] = data[i * batch_size + j: i * batch_size + j + window_size - 1, :]
             yield (x, y)
